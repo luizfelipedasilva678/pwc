4324018a43f1168634753764e23c7f31e81b3023
luizfelipe <luizfelipedasilva678@gmail.com>
2023-03-09 19:15:54 -0300

    Usando arguments

--- aula2/index.js
+++ aula2/index.js
@@ -1,3 +1,17 @@
 (function (x, y) {
   console.log(`Resultado ${x} elevado a ${y} = `, Math.pow(x, y));
 })(2, 3);
+
+function sum() {
+  let sum = 0;
+
+  const argumentos = Array.from(arguments);
+
+  argumentos.forEach((argumento) => {
+    sum += argumento;
+  });
+
+  return sum;
+}
+
+console.log("Resultado funcao sem argumentos: ", sum(2, 3, 4));
